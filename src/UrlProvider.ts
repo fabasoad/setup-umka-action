991f8fc3b85a1aadbf64aec8aea5be2db19f0bb8
Yevhen Fabizhevskyi <yevhen.fabizhevskyi@tri-ad.global>
2020-11-19 09:28:52 +0900

    Add "-alpha" suffix cases

--- src/UrlProvider.ts
+++ src/UrlProvider.ts
@@ -2,6 +2,8 @@ import { lt } from 'semver'
 import CliFileNameBuilder from './CliFileNameBuilder'
 
 export default class UrlProvider implements IUrlProvider {
+  private ALPHA_VERSION_BORDER: string = '0.3.0'
+
   private builder: ICliFileNameBuilder
   private version: string
 
@@ -13,7 +15,17 @@ export default class UrlProvider implements IUrlProvider {
   }
 
   getUrl(): string {
-    const suffix: string = lt(this.version, '0.3') ? '-alpha' : ''
+    let isAlpha: boolean = false
+    try {
+      isAlpha = lt(this.version, this.ALPHA_VERSION_BORDER)
+    } catch (e) {
+      if ((<Error>e).message === `Invalid Version: ${this.version}`) {
+        isAlpha = lt(`${this.version}.0`, this.ALPHA_VERSION_BORDER)
+      } else {
+        throw e
+      }
+    }
+    const suffix: string = isAlpha ? '-alpha' : ''
     return 'https://github.com/vtereshkov/umka-lang/releases/download/v' +
       `${this.version}${suffix}/${this.builder.build()}.zip`
   }
--- src/__tests__/UrlProvider.spec.ts
+++ src/__tests__/UrlProvider.spec.ts
@@ -35,8 +35,35 @@ describe('UrlProvider', () => {
       const actual: string = provider.getUrl()
       expect(actual).toBe('https://github.com/vtereshkov/umka-lang/releases/' +
       `download/v${expectedVersion}${item.suffix}/${fileName}.zip`)
-      semverLtStub.calledOnceWithExactly(expectedVersion, '0.3')
+      semverLtStub.calledOnceWithExactly(expectedVersion, '0.3.0')
     })
+
+  itParam('should throw error on invalid semver (${value.suffix})',
+    items, (item: IFixture) => {
+      const msg: string = `Invalid Version: ${expectedVersion}`
+      semverLtStub.onCall(0).throws(new Error(msg))
+      semverLtStub.onCall(1).returns(item.expectedSemverLt)
+      const fileName: string = '3iy81j7i'
+      const provider: UrlProvider = new UrlProvider(expectedVersion, {
+        build: (): string => fileName
+      })
+      const actual: string = provider.getUrl()
+      expect(actual).toBe('https://github.com/vtereshkov/umka-lang/releases/' +
+        `download/v${expectedVersion}${item.suffix}/${fileName}.zip`)
+      expect(semverLtStub.calledTwice).toBe(true)
+      semverLtStub.calledWithExactly(expectedVersion, '0.3.0')
+      semverLtStub.calledWithExactly(`${expectedVersion}.0`, '0.3.0')
+    })
+
+  it('should throw error on non-semver', () => {
+    const expectedError: Error = new Error('uj5p2t62')
+    semverLtStub.throws(expectedError)
+    const provider: UrlProvider = new UrlProvider(expectedVersion, {
+      build: (): string => '3iy81j7i'
+    })
+    expect(() => provider.getUrl()).toThrow(expectedError)
+    semverLtStub.calledOnceWithExactly(expectedVersion, '0.3.0')
+  })
 
   afterEach(() => restore())
 })
