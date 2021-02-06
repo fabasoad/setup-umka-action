045ca16b4047e066e62eb5a643e73121e2823ac4
fabasoad <fabasoad@gmail.com>
2021-02-06 18:05:11 +0900

    Fix tests

--- src/__tests__/UrlProvider.spec.ts
+++ src/__tests__/UrlProvider.spec.ts
@@ -1,6 +1,6 @@
 import itParam from 'mocha-param'
-import semver, { SemVer } from 'semver'
-import { restore, SinonStub, stub } from 'sinon'
+import { lt } from 'semver'
+import { restore } from 'sinon'
 import UrlProvider from '../UrlProvider'
 
 interface IFixture {
@@ -8,10 +8,12 @@ interface IFixture {
   suffix: string
 }
 
+jest.mock('semver', () => ({
+  lt: jest.fn()
+}))
+
 describe('UrlProvider', () => {
   const expectedVersion: string = 'lp9ec2wv'
-  let semverLtStub: SinonStub<
-    [v1: string | SemVer, v2: string | SemVer, loose?: boolean], boolean>
 
   const items: IFixture[] = [{
     suffix: '-alpha',
@@ -21,13 +23,13 @@ describe('UrlProvider', () => {
     lt: false
   }]
 
-  beforeEach(() => {
-    semverLtStub = stub(semver, 'lt')
+  afterEach(() => {
+    (lt as jest.Mock).mockClear()
   })
 
   itParam('should return url successfully (${value.lt})',
     items, (item: IFixture) => {
-      semverLtStub.returns(item.lt)
+      (lt as jest.Mock).mockImplementation(() => item.lt)
       const fileName: string = '3iy81j7i'
       const provider: UrlProvider = new UrlProvider(expectedVersion, {
         build: (): string => fileName
@@ -36,14 +38,16 @@ describe('UrlProvider', () => {
       expect(actual).toBe('https://github.com/vtereshkov/umka-lang/releases/' +
         `download/v${expectedVersion}${item.suffix}/` +
         `${fileName}.zip`)
-      expect(semverLtStub.withArgs(expectedVersion, '0.3.0').callCount).toBe(1)
+      expect(lt.mock.calls.length).toBe(1)
+      expect(lt).toHaveBeenCalledWith(expectedVersion, '0.3.0')
     })
 
   itParam('should throw error on invalid semver (${value.lt})',
     items, (item: IFixture) => {
-      const msg: string = `Invalid Version: ${expectedVersion}`
-      semverLtStub.onCall(0).throws(new Error(msg))
-      semverLtStub.onCall(1).returns(item.lt)
+      const msg: string = `Invalid Version: ${expectedVersion}`;
+      (lt as jest.Mock).mockImplementationOnce(() => {
+        throw new Error(msg)
+      }).mockImplementationOnce(() => item.lt)
       const fileName: string = '3iy81j7i'
       const provider: UrlProvider = new UrlProvider(expectedVersion, {
         build: (): string => fileName
@@ -52,21 +56,23 @@ describe('UrlProvider', () => {
       expect(actual).toBe('https://github.com/vtereshkov/umka-lang/releases/' +
         `download/v${expectedVersion}${item.suffix}/` +
         `${fileName}.zip`)
-      expect(semverLtStub.calledTwice).toBe(true)
-      expect(semverLtStub.withArgs(expectedVersion, '0.3.0').callCount).toBe(1)
-      expect(semverLtStub.withArgs(`${expectedVersion}.0`, '0.3.0').callCount)
-        .toBe(1)
+      expect(lt.mock.calls.length).toBe(2)
+      expect(lt).toHaveBeenCalledWith(expectedVersion, '0.3.0')
+      expect(lt).toHaveBeenCalledWith(`${expectedVersion}.0`, '0.3.0')
     })
 
   it('should throw error on non-semver', () => {
     const version: string = 'rs3cj88e'
-    const expectedError: Error = new Error('uj5p2t62')
-    semverLtStub.throws(expectedError)
+    const expectedError: Error = new Error('uj5p2t62');
+    (lt as jest.Mock).mockImplementation(() => {
+      throw expectedError
+    })
     const provider: UrlProvider = new UrlProvider(version, {
       build: (): string => '3iy81j7i'
     })
     expect(() => provider.getUrl()).toThrow(expectedError)
-    expect(semverLtStub.withArgs(version, '0.3.0').callCount).toBe(1)
+    expect(lt.mock.calls.length).toBe(1)
+    expect(lt).toHaveBeenCalledWith(version, '0.3.0')
   })
 
   afterEach(() => restore())
