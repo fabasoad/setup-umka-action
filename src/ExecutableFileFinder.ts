7b98c44db9cb44485056dbb69e53fa6d0d172401
fabasoad <fabasoad@gmail.com>
2022-05-16 17:29:31 +0900

    Debug 7

--- src/ExecutableFileFinder.ts
+++ src/ExecutableFileFinder.ts
@@ -1,5 +1,6 @@
 import glob from 'glob'
 import path from 'path'
+import { readdirSync, statSync } from 'fs'
 import { Logger } from 'winston'
 import CliExeNameProvider from './CliExeNameProvider'
 import { CLI_NAME } from './consts'
@@ -14,21 +15,19 @@ export default class ExecutableFileFinder implements IExecutableFileFinder {
     this.provider = provider
   }
 
-  find(folderPath: string): string {
-    const pattern: string =
-      `${folderPath}${path.sep}**${path.sep}${CLI_NAME}*`
-    let files: string[] = glob.sync(pattern)
-    this.log.info(`Files found by glob: ${files.join(', ')}`)
-    files = files
-      .filter((f: string) => f.endsWith(this.provider.getExeFileName()))
-    if (files.length === 0) {
-      throw new Error('Execution file has not been found under ' +
-        `${folderPath} folder using ${pattern} pattern`)
-    } else if (files.length > 1) {
-      throw new Error('There are more than 1 execution file has been found ' +
-        `under ${folderPath} folder using ${pattern} pattern: ${files}`)
+  find(dirPath: string): string {
+    const files: string[] = [dirPath]
+    while (files.length > 0) {
+      const filePath: string = files.pop() || ''
+      if (statSync(filePath).isDirectory()) {
+        readdirSync(filePath)
+          .forEach((f: string) => files.push(`${filePath}${path.sep}${f}`))
+      } else if (filePath.endsWith(this.provider.getExeFileName())) {
+        this.log.info(`${CLI_NAME} path is ${filePath}`)
+        return filePath
+      }
     }
-    this.log.info(`${CLI_NAME} path is ${files[0]}`)
-    return files[0]
+    throw new Error('Execution file has not been found under ' +
+      `${dirPath} folder`)
   }
 }
