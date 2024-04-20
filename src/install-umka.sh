533960653b6f3c24f8f23efb3725a8406a61b94b
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-20 22:59:36 +0900

    Test 9

--- src/install-umka.sh
+++ src/install-umka.sh
@@ -2,11 +2,17 @@
 
 # removing 'v' prefix: v1.2.3 -> 1.2.3
 tag="${1#v}"
+dir="$2"
+
+suffix=""
+if [ "${tag}" != "1.3" ] && [ "${tag}" != "1.3.1" ]; then
+  suffix="_x86-64"
+fi
 
 os="linux"
 if [ "${RUNNER_OS}" = "Windows" ]; then
   os="windows_mingw"
 else
-  chmod +x "${GITHUB_WORKSPACE}/umka/umka_${tag}_${os}/umka"
+  chmod +x "${GITHUB_WORKSPACE}/${dir}/umka_${tag}${suffix}_${os}/umka"
 fi
-echo "${GITHUB_WORKSPACE}/umka/umka_${tag}_${os}" >> "$GITHUB_PATH"
+echo "${GITHUB_WORKSPACE}/${dir}/umka_${tag}${suffix}_${os}" >> "$GITHUB_PATH"
