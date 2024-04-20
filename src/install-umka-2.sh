692c492b2f28f8d13d09a43224968d8017280776
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-20 23:11:09 +0900

    Test 12

--- src/install-umka-2.sh
+++ src/install-umka-2.sh
@@ -3,7 +3,7 @@
 # removing ".zip" from path
 path=${1%.*}
 ls -la "${path}"
-#if [ "${RUNNER_OS}" != "Windows" ]; then
-#  chmod +x "${path}/umka"
-#fi
+if [ "${RUNNER_OS}" != "Windows" ]; then
+  chmod +x "${path}/umka"
+fi
 echo "${path}" >> "$GITHUB_PATH"
