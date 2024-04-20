025540eb20b7577233ce588c16c217d6cebabaff
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-20 23:14:03 +0900

    Rollback

--- src/collect-info.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-installed="false"
-if command -v umka >/dev/null 2>&1; then
-  installed="true"
-fi
-
-echo "umka-installed=${installed}" >> "$GITHUB_OUTPUT"
--- src/install-umka-2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# removing ".zip" from path
-path=${1%.*}
-ls -la "${path}"
-if [ "${RUNNER_OS}" != "Windows" ]; then
-  chmod +x "${path}/umka"
-fi
-echo "${path}" >> "$GITHUB_PATH"
